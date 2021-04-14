ec2d20bc65bdc5fe973be2d76ed1e76ed6af64be
yegyeom <yaegyum99@gmail.com>
2021-04-14 19:52:37 +0900

    basic assignment

--- #B2/ssu_employee.h
+++
@@ -1,7 +0,0 @@
-#define NAME_SIZE 64
-
-struct ssu_employee{
-	char name[NAME_SIZE];
-	int pid;
-	int salary;
-};
--- #B5/ssu_oslab.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(){
-	printf("This is oslab file\n");
-	exit(0);
-}
